398d2c765ce439b70761fc827b4e54bc24a22133
weiwenwei <15027128561@163.com>
2021-11-09 13:58:54 +0800

    first commit

---
+++ src/main/java/com/wei/mall/model/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.wei.mall.model.dao;
+
+import com.wei.mall.model.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
---
+++ src/main/java/com/wei/mall/model/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.wei.mall.model.dao;
+
+import com.wei.mall.model.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
